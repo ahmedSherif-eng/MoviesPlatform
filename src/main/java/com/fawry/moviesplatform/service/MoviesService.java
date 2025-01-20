61f21f21268a3feba2bbe4f65cc6f8a8f34ae690
VEX <ahmedsherif000035@gmail.com>
2025-01-21 00:21:02 +0200

    add paging method to Service layer

--- src/main/java/com/fawry/moviesplatform/service/MoviesService.java
+++ src/main/java/com/fawry/moviesplatform/service/MoviesService.java
@@ -1,14 +1,17 @@
 package com.fawry.moviesplatform.service;
 
+import com.fawry.moviesplatform.DTO.MovieDTO;
 import com.fawry.moviesplatform.entity.Movie;
 import com.fawry.moviesplatform.entity.OmdbSearchResult;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface MoviesService {
     OmdbSearchResult searchMovies(String query);
-    Movie addMovie(Movie movie);
+    Movie addMovie(MovieDTO movieDTO);
     void removeMovie(Long id);
-    List<Movie> getAllMovies();
+    Page<MovieDTO> getMoviesPage(Pageable pageable);
     Movie getMovieDetails(Long id);
 }
