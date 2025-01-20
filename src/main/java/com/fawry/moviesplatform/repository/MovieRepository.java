2022f78c0bb97b291e3d9819addde93bc679a841
VEX <ahmedsherif000035@gmail.com>
2025-01-21 00:19:59 +0200

    add needed methods of JPA repo

--- src/main/java/com/fawry/moviesplatform/repository/MovieRepository.java
+++ src/main/java/com/fawry/moviesplatform/repository/MovieRepository.java
@@ -1,7 +1,16 @@
 package com.fawry.moviesplatform.repository;
 
 import com.fawry.moviesplatform.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MovieRepository extends JpaRepository<Movie,Long> {
+    Optional<Movie> findById(Long Id);
+    void deleteById(Long Id);
+    Page<Movie> findAll(Pageable pageable);
+
+
 }
