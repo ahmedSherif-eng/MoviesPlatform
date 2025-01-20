048d32989850146ae8a29cca46f3d4bded00b5b6
VEX <ahmedsherif000035@gmail.com>
2025-01-21 00:22:17 +0200

    implement Movies Mapper class

---
+++ src/main/java/com/fawry/moviesplatform/mapper/MoviesMapper.java
@@ -0,0 +1,32 @@
+package com.fawry.moviesplatform.mapper;
+
+import com.fawry.moviesplatform.DTO.MovieDTO;
+import com.fawry.moviesplatform.entity.Movie;
+
+public class MoviesMapper {
+    public static Movie mapToMovie(MovieDTO movieDTO){
+        if (movieDTO == null) {
+            return null;
+        }
+        Movie movie = new Movie();
+        movie.setTitle(movieDTO.getTitle());
+        movie.setType(movieDTO.getType());
+        movie.setYear(movieDTO.getYear());
+        movie.setImdbID(movieDTO.getImdbID());
+        movie.setPoster(movieDTO.getPoster());
+        return movie;
+    }
+    public static MovieDTO mapToMovieDTO(Movie movie) {
+        if (movie == null) {
+            return null;
+        }
+
+        MovieDTO movieDTO = new MovieDTO();
+        movieDTO.setImdbID(movie.getImdbID());
+        movieDTO.setPoster(movie.getPoster());
+        movieDTO.setTitle(movie.getTitle());
+        movieDTO.setType(movie.getType());
+        movieDTO.setYear(movie.getYear());
+        return movieDTO;
+    }
+}
