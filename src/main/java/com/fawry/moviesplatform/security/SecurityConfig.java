163542e48451b786f09f938099233c805a468761
VEX <ahmedsherif000035@gmail.com>
2025-01-21 00:23:35 +0200

    disable csrf for testing purpose

--- src/main/java/com/fawry/moviesplatform/security/SecurityConfig.java
+++ src/main/java/com/fawry/moviesplatform/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,7 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.
+        http.csrf(AbstractHttpConfigurer::disable).
                 authorizeHttpRequests(auth -> auth
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/public/**").hasAnyRole("USER", "ADMIN")
