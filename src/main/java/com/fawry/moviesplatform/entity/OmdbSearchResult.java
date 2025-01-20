4d4e0243256533dc55d92cf98f125a430c72735f
VEX <ahmedsherif000035@gmail.com>
2025-01-21 00:23:12 +0200

    refactor OmdbSearchResult class

--- src/main/java/com/fawry/moviesplatform/entity/OmdbSearchResult.java
+++ src/main/java/com/fawry/moviesplatform/entity/OmdbSearchResult.java
@@ -1,12 +1,25 @@
 package com.fawry.moviesplatform.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fawry.moviesplatform.DTO.MovieDTO;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 import java.util.List;
 
 @Setter
 @Getter
+@NoArgsConstructor
 public class OmdbSearchResult {
- private List<Movie> SearchResult;
+ @JsonProperty("Search")
+ private List<MovieDTO> Search;
+
+ @JsonProperty("totalResults")
+ private String totalResults;
+
+ @JsonProperty("Response")
+ private String Response;
 }
