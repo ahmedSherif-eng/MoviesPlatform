d53e6b50efbca3f024b10dffdfda33af1cd89ada
VEX <ahmedsherif000035@gmail.com>
2025-01-22 19:21:41 +0200

    implement CustomUserDetailsService.java to fetch credentials from database

---
+++ src/main/java/com/fawry/moviesplatform/security/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.fawry.moviesplatform.security;
+
+import com.fawry.moviesplatform.security.DTO.Roles;
+import com.fawry.moviesplatform.security.DTO.Users;
+import com.fawry.moviesplatform.security.SecurityRepo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Fetch the user from the database
+        Users user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+                .collect(Collectors.toList());
+                
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                authorities
+        );
+    }
+}
+
