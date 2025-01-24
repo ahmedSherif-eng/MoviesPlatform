599d38f1e264ffeba0155895b259eba556f0ae5b
VEX <ahmedsherif000035@gmail.com>
2025-01-25 00:17:28 +0200

    Implement MovieDetailsDTO

---
+++ src/main/java/com/fawry/moviesplatform/DTO/MovieDetailsDTO.java
@@ -0,0 +1,88 @@
+package com.fawry.moviesplatform.DTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class MovieDetailsDTO {
+    @JsonProperty("Title")
+    private String title;
+
+    @JsonProperty("Year")
+    private String year;
+
+    @JsonProperty("Rated")
+    private String rated;
+
+    @JsonProperty("Released")
+    private String released;
+
+    @JsonProperty("Runtime")
+    private String runtime;
+
+    @JsonProperty("Genre")
+    private String genre;
+
+    @JsonProperty("Director")
+    private String director;
+
+    @JsonProperty("Writer")
+    private String writer;
+
+    @JsonProperty("Actors")
+    private String actors;
+
+    @JsonProperty("Plot")
+    private String plot;
+
+    @JsonProperty("Language")
+    private String language;
+
+    @JsonProperty("Country")
+    private String country;
+
+    @JsonProperty("Awards")
+    private String awards;
+
+    @JsonProperty("Poster")
+    private String poster;
+
+    @JsonProperty("Metascore")
+    private String metaScore;
+
+    @JsonProperty("imdbRating")
+    private String imdbRating;
+
+    @JsonProperty("imdbVotes")
+    private String imdbVotes;
+
+    @JsonProperty("imdbID")
+    private String imdbID;
+
+    @JsonProperty("Type")
+    private String type;
+
+    @JsonProperty("DVD")
+    private String dvd;
+
+    @JsonProperty("BoxOffice")
+    private String boxOffice;
+
+    @JsonProperty("Production")
+    private String production;
+
+    @JsonProperty("Website")
+    private String website;
+
+    @JsonProperty("Response")
+    private boolean response;
+
+    @JsonProperty("Ratings")
+    private List<RatingDTO> ratings;
+}
