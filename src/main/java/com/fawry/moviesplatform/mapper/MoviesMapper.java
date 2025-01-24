e566b1ea1fb70ac77b5c6b65d8ddbad920bdd25d
VEX <ahmedsherif000035@gmail.com>
2025-01-25 00:21:31 +0200

    Refactor MoviesMapper to map to movie with full movie details

--- src/main/java/com/fawry/moviesplatform/mapper/MoviesMapper.java
+++ src/main/java/com/fawry/moviesplatform/mapper/MoviesMapper.java
@@ -1,19 +1,48 @@
 package com.fawry.moviesplatform.mapper;
 
 import com.fawry.moviesplatform.DTO.MovieDTO;
+import com.fawry.moviesplatform.DTO.MovieDetailsDTO;
 import com.fawry.moviesplatform.entity.Movie;
+import com.fawry.moviesplatform.entity.Rating;
+
+import java.util.stream.Collectors;
 
 public class MoviesMapper {
-    public static Movie mapToMovie(MovieDTO movieDTO){
-        if (movieDTO == null) {
+    public static Movie mapToMovie(MovieDetailsDTO movieDetailsDTO){
+        if (movieDetailsDTO == null) {
             return null;
         }
+
         Movie movie = new Movie();
-        movie.setTitle(movieDTO.getTitle().toLowerCase());
-        movie.setType(movieDTO.getType());
-        movie.setYear(movieDTO.getYear());
-        movie.setImdbID(movieDTO.getImdbID());
-        movie.setPoster(movieDTO.getPoster());
+        movie.setImdbID(movieDetailsDTO.getImdbID());
+        movie.setPoster(movieDetailsDTO.getPoster());
+        movie.setTitle(movieDetailsDTO.getTitle());
+        movie.setType(movieDetailsDTO.getType());
+        movie.setYear(movieDetailsDTO.getYear());
+        movie.setActors(movieDetailsDTO.getActors());
+        movie.setAwards(movieDetailsDTO.getAwards());
+        movie.setCountry(movieDetailsDTO.getCountry());
+        movie.setDirector(movieDetailsDTO.getDirector());
+        movie.setGenre(movieDetailsDTO.getGenre());
+        movie.setLanguage(movieDetailsDTO.getLanguage());
+        movie.setPlot(movieDetailsDTO.getPlot());
+        movie.setProduction(movieDetailsDTO.getProduction());
+        movie.setRated(movieDetailsDTO.getRated());
+        movie.setReleased(movieDetailsDTO.getReleased());
+        movie.setRuntime(movieDetailsDTO.getRuntime());
+        movie.setWriter(movieDetailsDTO.getWriter());
+        movie.setImdbRating(movieDetailsDTO.getImdbRating());
+        movie.setImdbVotes(movieDetailsDTO.getImdbVotes());
+        movie.setRatings(movieDetailsDTO.getRatings().stream()
+                .map(ratingDTO -> {
+                    Rating rating = new Rating();
+                    rating.setMovie(movie);
+                    rating.setSource(ratingDTO.getSource());
+                    rating.setValue(ratingDTO.getValue());
+                    return rating;
+                })
+                .collect(Collectors.toList()));
+
         return movie;
     }
     public static MovieDTO mapToMovieDTO(Movie movie) {
