11d1529b5941fc5bf1c338340b99adf2b744d698
VEX <ahmedsherif000035@gmail.com>
2025-01-25 00:22:07 +0200

    Add Movie Details endpoint to OmdbConst

--- src/main/java/com/fawry/moviesplatform/constants/OmdbConst.java
+++ src/main/java/com/fawry/moviesplatform/constants/OmdbConst.java
@@ -2,5 +2,6 @@ package com.fawry.moviesplatform.constants;
 
 public class OmdbConst {
     private static final String OMDB_API_KEY = "a3fea2ab";
-    public static final String OMDB_URL = "http://www.omdbapi.com/?apikey=" + OMDB_API_KEY + "&s=";
+    public static final String OMDB_Movie_URL = "http://www.omdbapi.com/?apikey=" + OMDB_API_KEY + "&s=";
+    public static final String OMDB_Movie_Details_URL = "http://www.omdbapi.com/?apikey=" + OMDB_API_KEY + "&i=";
 }
