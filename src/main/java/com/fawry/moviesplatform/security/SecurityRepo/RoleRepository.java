713e5131c9f41bca130bf427399dd7beddd22d30
VEX <ahmedsherif000035@gmail.com>
2025-01-25 00:23:19 +0200

    Implement RoleRepository

---
+++ src/main/java/com/fawry/moviesplatform/security/SecurityRepo/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.fawry.moviesplatform.security.SecurityRepo;
+
+import com.fawry.moviesplatform.security.DTO.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Roles, Long> {
+    Optional<Roles> findByName(String name);
+}
