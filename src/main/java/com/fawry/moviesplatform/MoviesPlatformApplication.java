ba1ee244e203a70c6c58150d3c4a19122c2fbf3a
VEX <ahmedsherif000035@gmail.com>
2025-01-25 18:22:59 +0200

    Spring boot entry point

---
+++ src/main/java/com/fawry/moviesplatform/MoviesPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.fawry.moviesplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesPlatformApplication.class, args);
+    }
+
+}
