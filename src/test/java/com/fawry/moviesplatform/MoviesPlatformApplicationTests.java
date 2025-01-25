dff69e961ba13a1f9cd43b68f8435212926f4c95
VEX <ahmedsherif000035@gmail.com>
2025-01-25 18:22:15 +0200

    Spring boot entry point

---
+++ src/test/java/com/fawry/moviesplatform/MoviesPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fawry.moviesplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
