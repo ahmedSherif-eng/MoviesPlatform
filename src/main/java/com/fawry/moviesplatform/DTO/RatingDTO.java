d0aba604ad0b5da085e87b18069b23a4c17c88d0
VEX <ahmedsherif000035@gmail.com>
2025-01-25 21:01:51 +0200

    Add @AllArgsConstructor in RatingDTO

--- src/main/java/com/fawry/moviesplatform/DTO/RatingDTO.java
+++ src/main/java/com/fawry/moviesplatform/DTO/RatingDTO.java
@@ -1,6 +1,7 @@
 package com.fawry.moviesplatform.DTO;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class RatingDTO {
 
     @JsonProperty("Source")
