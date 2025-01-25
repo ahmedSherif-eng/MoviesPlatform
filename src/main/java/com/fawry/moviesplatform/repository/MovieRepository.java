eccba4227af0ec3abb54125a9c84316fa0fb58bc
VEX <ahmedsherif000035@gmail.com>
2025-01-25 16:25:15 +0200

    Refactor MovieRepository remove unnecessary code

--- src/main/java/com/fawry/moviesplatform/repository/MovieRepository.java
+++ src/main/java/com/fawry/moviesplatform/repository/MovieRepository.java
@@ -5,27 +5,21 @@ import lombok.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface MovieRepository extends JpaRepository<Movie,Long> {
-    Optional<Movie> findById(Long Id);
-
-    void deleteById(Long Id);
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     Optional<Movie> findByImdbID(String imdbID);
 
     @Query("SELECT m.imdbID FROM Movie m")
     List<String> findAllIds();
 
-    Page<Movie> findAll(Pageable pageable);
+    Page<Movie> findByTitleContaining(@NonNull String title, Pageable pageable);
 
-
-    Page<Movie> findByTitleContaining(String title, Pageable pageable);
-
-    @Modifying
+    @Transactional
     void deleteByTitle(@NonNull String title);
 }
