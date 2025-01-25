c704d98c713508d2583157f852898c933eb05835
VEX <ahmedsherif000035@gmail.com>
2025-01-25 18:19:02 +0200

    Implement Signature Exception handler in GlobalExceptionHandler

--- src/main/java/com/fawry/moviesplatform/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fawry/moviesplatform/exception/GlobalExceptionHandler.java
@@ -6,9 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
     
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
