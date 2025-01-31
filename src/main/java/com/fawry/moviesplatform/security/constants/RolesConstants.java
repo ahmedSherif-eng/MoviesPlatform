2b2c98a4746d228095ac9c58e2b032f289a8b895
VEX <72231218+ahmedSherif-eng@users.noreply.github.com>
2025-01-31 17:11:35 +0200

    Implement RolesConstants class

---
+++ src/main/java/com/fawry/moviesplatform/security/constants/RolesConstants.java
@@ -0,0 +1,6 @@
+package com.fawry.moviesplatform.security.constants;
+
+public class RolesConstants {
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+}
